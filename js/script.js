343437db389c04deb47c7b9d0d4aeaec3cdc3d95
Rafael Kimura <rafaelkimura94@gmail.com>
2020-05-28 20:31:11 -0300

    startpage implemented using jquery and bootstrap

--- js/script.js
+++ js/script.js
@@ -1,50 +1,15 @@
-function myGreeting() {
+var today = new Date();
+var time = today.getHours();
 
-    var today = new Date();
-    var time = today.getHours();
-    
-    var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    
-    document.getElementById("calendar").innerHTML = week[today.getDay()] + ", " + month[today.getMonth()] + " " + today.getDate();
+var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-    if (time >= 6 && time < 12) {
-        document.getElementById("greeting").innerHTML = "Good Morning, Kimura!";
-    } else if (time >= 12 && time < 17) {
-        document.getElementById("greeting").innerHTML = "Good Afternoon, Kimura!";
-    } else {
-        document.getElementById("greeting").innerHTML = "Good Evening, Kimura!";
-    }
+$("#calendar").html(week[today.getDay()] + ", " + month[today.getMonth()] + " " + today.getDate());
 
+if (time >= 6 && time < 12) {
+    $("#greeting").html("Good Morning, Kimura!");
+} else if (time >= 12 && time < 17) {
+    $("#greeting").html("Good Afternoon, Kimura!");
+} else {
+    $("#greeting").html("Good Evening, Kimura!");
 }
-
-function sameHeight() {
-
-    var h = new Array(3);
-    h[0] = document.getElementById("social-box").clientHeight;
-    h[1] = document.getElementById("university-box").clientHeight;
-    h[2] = document.getElementById("programming-box").clientHeight;
-
-    var max_h = h[0];
-    
-    /* Find the biggest h[i] */
-    for( var i = 1; i < h.length; i ++) {
-        if (h[i] > max_h) {
-            max_h = h[i];
-        }
-    }
-
-    /* Resizes every class element to biggest h[i] */
-    var divs = document.getElementsByClassName("content-box");
-    for( var j = 0; j < divs.length; j++) {
-        divs[j].style.height = max_h + "px";
-    }
-
-}
-
-myGreeting();
-
-sameHeight();
-
-
-
