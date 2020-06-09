fd2748d3b5a7bfb0c10b19754e27f8b2951e96a8
Rafael Kimura <rafaelkimura94@gmail.com>
2020-06-09 10:21:01 -0300

    removed jquery and bootstrap from code

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-var today = new Date();
-var time = today.getHours();
+var today = new Date()
+var time = today.getHours()
 
-var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
-$("#calendar").html(week[today.getDay()] + ", " + month[today.getMonth()] + " " + today.getDate());
+document.getElementById("calendar").innerHTML = week[today.getDay()] + ", " + month[today.getMonth()] + " " + today.getDate()
 
 if (time >= 6 && time < 12) {
-    $("#greeting").html("Good Morning, Kimura!");
+    document.getElementById("greeting").innerHTML = "Good Morning, Kimura"
 } else if (time >= 12 && time < 17) {
-    $("#greeting").html("Good Afternoon, Kimura!");
+    document.getElementById("greeting").innerHTML = "Good Afternoon, Kimura"
 } else {
-    $("#greeting").html("Good Evening, Kimura!");
+    document.getElementById("greeting").innerHTML = "Good Evening, Kimura"
 }
